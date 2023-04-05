8e9923c55ffae9d24bb2da7437606f06158dd613
Desi Beam <desiibeam@yahoo.com>
2023-04-05 02:10:34 +0000

    List

---
+++ list.py
@@ -0,0 +1,21 @@
+var = []
+
+print(type(var))
+
+var2 = [256, 494, 564, 321]
+
+print(var2)
+
+var3 = [256, 494, 564, 321, 456, 789, 147, 333]
+
+print(var3)
+
+var4 = [256, 494, 564, 321, "007"]
+
+print(var4)
+
+var3.append(494)
+
+print(var3)
+
+print(dir(var3))
